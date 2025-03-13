c2fcbc67338ae9544ce65745b40bc4330bd207fc
AshwiniAgarkar <163733901+AshwiniAgarkar@users.noreply.github.com>
2025-03-13 22:04:11 +0530

    Create assignment1.py

---
+++ assignment1.py
@@ -0,0 +1,45 @@
+#question 1
+# mile1=5280 
+# mile13=5280*13
+# print("13 mile is equal to",mile13)
+
+#question 2
+# time=(7*3600) + (21*60) +37
+# print("7 hours 21 minutes 37 seconds is equal to",time,"seconds")
+
+#question 3
+# w=4
+# l=7
+# perimeter= 2*w + 2*l
+# print("perimeter is",perimeter,"inches")
+
+#question 4
+# radius= 8
+# circumference=2*3.14*radius
+# print("circumference of circle is",circumference,"inches")
+
+#question 5
+# y=10
+# r=7
+# p=1000
+# money= p*(1+0.01*r)*y
+# print(money)
+
+#question 6
+# str1="SDP guruji is"
+# str2=" 27 "
+# str3=" years old."
+# str=str1+str2+str3
+# print(str)
+
+#question 7
+# str1= "My name is "
+# str2=" SDP"
+# str3=" GURUJI"
+# print(str1+str2+str3)
+
+#question 8
+# x0,y0=2,2
+# x1,y1=5,6
+# distance= (x1- x0)*2 + (y1- y0)*2
+# print("distance is",distance)
