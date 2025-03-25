28b41042710193ff8ee866834056a23a3f7f7a13
AshwiniAgarkar <163733901+AshwiniAgarkar@users.noreply.github.com>
2025-03-25 20:19:03 +0530

    Add files via upload

---
+++ assignment 8.py
@@ -0,0 +1,45 @@
+#QUESTION 1
+# my_tuple=(1,2,3,4,6,90,55,33,43,65)
+# print(sorted(my_tuple))
+
+
+#QUESTION 2
+# num=(1,2,3,4,6,90,55,33,43,65)
+# sum=sum(num)
+# print("the sum of all elements is:"sum)
+
+
+#QUESTION 3
+# elements=(1,2,3,4,6,90,55,33,43,65)
+# max_val=max(elements)
+# min_val=min(elements)
+# print("maximum of elements is:",max_val)
+# print("minimum of elements is:",min_val)
+
+
+#QUESTION 4
+# elements=(1,1,2,3,4,6,90,1,33,43,65,90)
+# upper_range=90
+# lower_range=20
+# count=0
+# for i in elements:
+#     if lower_range <= i <= upper_range:
+#       count+=1
+# print(count)
+
+
+#QUESTION 5
+# my_tuple=(1,1,2,3,4,6,90,1,33,43,65,90)
+# reversed_tuple=my_tuple[::-1]
+# print(reversed_tuple)
+
+#QUESTION 6
+# my_tuple=(1,1,2,3,4,6,90,1,33,43,65,9.70)
+# for i in my_tuple:
+#     print(len(str(i)))
+
+
+# my_tuple = (1, 2, 3, 4, 5)
+# print(my_tuple[1.5]) 
+# #FLOATING NUMBER IN THE INDEX WILL RAISE AN ERROR
+
---
+++ assignment.6_7.py
@@ -0,0 +1,54 @@
+#QUESTION 1
+# lists=["apple",2,800,"mango","pen","pencil"]
+# lists[3]="jackfruit"
+# print("updated list is",lists)
+# print(len(lists))
+
+#QUESTION 2
+# numbers=[10,20,30,40,50,60,70,80]
+# numbers.append(90)
+# numbers.append(100)
+# print(numbers)
+
+#QUESTION 3
+# my_list=[78,98,56,9,45,23,11,15,2]
+# my_list.sort()
+# print(my_list)
+
+#QUESTION 4
+# my_list=[78,98,56,9,45,23,11,15,2]
+# totalsum=sum(my_list)
+# print("sum of all the elements in list is",totalsum)
+
+#QUESTION 6
+# Create a list of numbers
+# my_list = [10,20,30,40,50,60,70]
+# max_value = max(my_list)
+# min_value = min(my_list)
+# print("Maximum value:", max_value)
+# print("Minimum value:", min_value)
+
+#QUESTION 5
+# lst=([5,5,5,5])
+# if lst[0]==lst[1] and lst[1]==lst[2] and lst[2]==lst[3]:
+#         print("All elements are the same.")
+# else:
+#     print("The elements are not the same.")
+
+#QUESTION 7
+# fruit="banana"
+# count=0
+# for i in fruit:
+#     if i=="a":
+#         count+=1
+# print(count)
+
+
+# #QUESTION 8
+#fruits = ["apple", "banana", "grape", "orange", "kiwi", "cherry", "mango", "pear"]
+#print(fruits[::-1])
+
+#QUESTION 9
+# fruits = ["apple", "banana", "grape", "orange", "kiwi", "cherry", "mango", "pear"]
+# for i in fruits:
+#     print(len(i))
